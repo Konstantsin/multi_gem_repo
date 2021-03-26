818e692ef0edb081d91ef5b2cee83135b1d3c386
Konstantin Savon <konstantin.savon@aterise.com>
2021-03-26 17:23:44 +0300

    Add firrst test gem

---
+++ firsttestgem/lib/firsttestgem.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "firsttestgem/version"
+
+module Firsttestgem
+  class Error < StandardError; end
+
+  def self.run
+    puts "Executing first test gem"
+  end
+end
